13413ce645690d73b96ce735aafa8441815e98f3
ALMSantana <andrelms4@insper.edu.br>
2025-03-31 16:05:03 -0300

    Aula 4.3

--- avaliacao.py
+++ avaliacao.py
@@ -2,6 +2,7 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import pandas as pd
 import os
+import time
 
 def avaliar_perfil_usuario(comentarios, client):
     system = """
@@ -23,7 +24,7 @@ def avaliar_perfil_usuario(comentarios, client):
         ]
     )
 
-    response.choices[0].message.content
+    return response.choices[0].message.content
 
 def carrega_csv(caminho_dados):
     try:
@@ -33,9 +34,46 @@ def carrega_csv(caminho_dados):
         print("Não abriu o Data Frame, erro: ", erro)
         return None
 
+def agrupar_comentarios_por_usuario(data_frame_cinema):
+    resultados = []
+    grupos = data_frame_cinema.groupby("user_id")
 
+    for user_id, grupo in grupos:
+        nome = grupo.iloc[0]["nome_completo"]
+        comentarios = "\n".join(grupo["comentario"].dropna().tolist())
+        resultados.append(
+            {
+                "user_id" : user_id,
+                "nome" : nome,
+                "comentarios" : comentarios
+            }
+        )
 
-load_dotenv()
+    return resultados
 
-openai_api_key = os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key=openai_api_key)
+def main():
+    load_dotenv()
+
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+    client = OpenAI(api_key=openai_api_key)
+
+    data_frame_cinema = carrega_csv("dados/comentarios.csv")
+    usuarios = agrupar_comentarios_por_usuario(data_frame_cinema=data_frame_cinema)
+
+    resultado_avaliacao = []
+    for usuario in usuarios:
+        parecer = avaliar_perfil_usuario(usuario["comentarios"], client)
+
+        resultado_avaliacao.append(
+            {
+                "user_id" : usuario["user_id"],
+                "nome" : usuario["nome"],
+                "parecer" : parecer
+            }
+        )
+        print(f"Usuário {usuario["nome"]}")
+        print(f"Parecer: {parecer}")
+        time.sleep(50)
+
+if __name__ == "__main__":
+    main()
